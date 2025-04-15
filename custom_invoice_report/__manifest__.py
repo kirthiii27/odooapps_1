5c7cb1e0c2643827f7b06cd5c00e79e160b48f5a
sarath-ksa <saraathkumar21@gmail.com>
2025-04-15 16:48:23 +0530

    [ADD]the upload the module

---
+++ custom_invoice_report/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Custom Invoice Report 80 mm',
+    'version': '1.0',
+    'category': 'Accounting',
+    'summary': 'Custom invoice report with outstanding credits  amount and Post Dated  cheques amount',
+    'description': 'This module customizes the invoice report to include outstanding credits amount  and post-dated cheques amount .The Invoice PDF report is 80mm',
+    'sequence': 1,
+    'author': 'Alan Technologies',
+    'company': 'Alan Technologies',
+    'maintainer': 'Alan Technologies',
+    'website': "https://alantechnologies.in/",
+    'depends': ['account', 'base'],
+    'data': [
+        'models/model_fields.xml',
+        'views/report_view.xml',
+        'actions/report_action.xml',
+
+    ],
+
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
