0338f64cf5f21249df6d103b235b471ca4e9fa19
Dharshini <dharshini.alantechnology@gmail.com>
2025-08-19 17:29:45 +0530

    Fix module folder structure by removing nested pet_clinic_management folder

--- pet_clinic_management/pet_clinic_management/models/medicine.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class Medicine(models.Model):
-    _name = 'vet.medicine'
-    _description = 'Medicine'
-
-    name = fields.Char(string='Medicine Name', required=True)
-    description = fields.Text(string='Description')
